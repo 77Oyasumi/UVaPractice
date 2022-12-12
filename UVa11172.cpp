36f0193377db257ab92779c0097ccf8f09ea4482
Oyasumi <xihan1205@gamil.com>
2022-12-13 02:04:22 +0800

    12/13

---
+++ UVa11172.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,testCase,i;
+    cin>>testCase;
+    for(i = 0; i < testCase; i++){
+        cin>>a>>b;
+        if(a < b){
+            cout<<"<"<<endl;
+        }else if (a > b){
+            cout<<">"<<endl;
+        }else{
+            cout<<"="<<endl;
+        }
+        
+    }
+}

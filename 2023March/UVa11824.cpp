5211bd80f612793fa9bd19887f522ca66b4379b8
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-04-24 16:19:51 +0800

    finish 11824

--- 2023March/UVa11824.cpp
+++ 2023March/UVa11824.cpp
@@ -18,7 +18,21 @@ int main(){
 
         sort(L, L+n);
 
-        
+        // "0LL"的作用是初始化變量"ans"為0，同時指定變量類型為"long long"
+        long long ans = 0LL, pow;
+
+        for(int i = n-1; i >= 0; --i){
+            pow = 2LL;
+            for(int j = n; j > i; --j){
+                pow = pow*L[i];
+            }
+            ans += pow;
+        }
+        if ( ans > 5000000){
+            printf("Too expensive\n");
+        }else{
+            printf("%lld\n", ans);
+        }
     }
     return 0;
 }

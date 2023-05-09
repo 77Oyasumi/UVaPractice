ef4b76dfb8ded41665f4b23842631d433240842d
Oyasumi <xihan1205@gmail.com>
2023-05-09 20:09:05 +0800

    5/9

---
+++ 2023May/UVa10783.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, i, a, b, c=0, k, sum;
+    // 輸入資料組數
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        // 輸入每組資料的起始與最終值
+        scanf("%d%d", &a, &b);
+        // 如果起始值 a 是奇數, 取 k = a
+        if(a%2 == 1){
+            k = a;
+        }else{ // 否則 k = a + 1
+            k = a + 1;
+        }
+        // 計算由 a 到 b 之間的奇數的總和
+        for(sum = 0; k <= b; k+=2){
+            sum += k;
+        }
+        // 將 c 加 1 之後, 輸出資料編號與奇數的總和
+        printf("Case %d: %d\n", ++c, sum);
+    }
+    return 0;
+}
---
+++ 2023May/UVa10812.cpp
@@ -0,0 +1,27 @@
+// (和 + 差) / 2 = 大數
+// (和 - 差) / 2 = 小數
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, i, s, d;
+    // 輸入資料的組數 n
+    scanf("%d", &n);
+    // 計算 n 組資料的結果
+    for(i = 0; i < n; i++){
+        //讀入兩隊的分數總和s和差d
+        scanf("%d%d", &s, &d);
+        // 檢查輸入資料如果有誤, 就輸出impossible, 錯誤狀況 :
+        // (1) 和 < 差
+        // (2) 和 < 0 or 差 < 0
+        // (3) (和+差) = 奇數
+        if(s<d||s<0||d<0||(s+d)%2==1){
+            printf("impossible\n");
+        }else{
+            printf("%d %d\n", (s+d)/2, (s-d)/2);
+        }
+    }
+    return 0;
+}
